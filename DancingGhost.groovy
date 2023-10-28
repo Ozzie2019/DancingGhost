6104a9b23810da838c656c758ab9ec88374b8edb
CAD User <CAD User@VM-898623>
2023-10-28 14:59:40 -0400

    Added cover and screw inserts.
    To have a cover for the servo when put into baseplate.

--- DancingGhost.groovy
+++ DancingGhost.groovy
@@ -4,7 +4,10 @@ import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
 double height = 15
 double width = 40
-BasePlate = new Cube(20, width, height)
+double length = 20
+double insety = 6
+double insetx = 4
+BasePlate = new Cube(length, width, height)
 		.toCSG()
 		.toZMin()
 		.toYMin()
@@ -16,6 +19,19 @@ CSG Servo = Vitamins.get("hobbyServo", "mg92b")
 		.toZMax()
 		.movez(height)
 		.movey(width)
-BasePlate = BasePlate.difference(Servo)
-		
-return [BasePlate]
+CSG Screw = Vitamins.get("chamferedScrew", "M3x16")
+		.toZMin()
+		.move(insetx, insety+2, 0)
+CSG Screw2 = Screw.movey(width-insety-5)
+BasePlate = BasePlate.difference([Servo,Screw,Screw2])
+CSG Cover = new Cube(length, 35, 2)
+		.toCSG()
+		.toZMin()
+		.toYMin()
+		.toXMin()
+		.movez(height)
+		.movey(5)
+Cover = Cover.difference([Screw,Screw2])
+CSG text = CSG.text("My Text",2,2)
+		.movez(height+2)
+return [BasePlate,Cover,text]
