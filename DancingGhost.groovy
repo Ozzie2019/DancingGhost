216d77077993be10d15e051fac592cab8e9dedb9
CAD User <CAD User@VM-898623>
2023-10-28 14:12:05 -0400

    Added Baseplate
    To have a Baseplate.

--- DancingGhost.groovy
+++ DancingGhost.groovy
@@ -1,1 +1,21 @@
-// code here
+// https://github.com/BancroftSchoolOpenSource/LetsPrintRobots-Courseware/releases
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+double height = 15
+double width = 40
+BasePlate = new Cube(20, width, height)
+		.toCSG()
+		.toZMin()
+		.toYMin()
+		.toXMin()
+CSG Servo = Vitamins.get("hobbyServo", "mg92b")
+		.roty(90)
+		.toXMin()
+		.toYMax()
+		.toZMax()
+		.movez(height)
+		.movey(width)
+BasePlate = BasePlate.difference(Servo)
+		
+return [BasePlate]
